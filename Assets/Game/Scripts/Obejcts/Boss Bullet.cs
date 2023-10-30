ad717afdbce1a5d7775f544cfc473c429cdb20cd
geunhi <105274692+geunhi@users.noreply.github.com>
2023-10-30 20:01:15 +0900

    Test Scene 만듬
    ㅇㅇ

---
+++ Assets/Game/Scripts/Obejcts/Boss Bullet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Boss_Bullet : Object
+{
+    Animator animator;
+
+    bool isDead;
+
+    private float aliveTime; // 스킬 생존 시간을 체크할 변수
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(!isDead) 
+        {
+            X += 10f * Time.deltaTime;
+
+            if (X >= 5)
+            {
+                bool destroySkill = aliveTime > 0.3f;
+
+                if (destroySkill)
+                {
+                    StartCoroutine(Dead());
+                    StopCoroutine(Dead());
+                    return;
+                }
+
+                aliveTime += Time.deltaTime;
+            }
+        }
+    }
+
+    IEnumerator Dead()
+    {
+        animator.SetTrigger("Hit");
+
+        isDead = true;
+
+        yield return new WaitForSeconds(0.35f); // 지정한 초 만큼 쉬기
+
+        Destroy(gameObject);
+    }
+}
+
