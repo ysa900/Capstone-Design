cde43ba8e1950088d7aa87ae88b0469df90e7d66
HyeongSeok11 <63270305+HyeongSeok11@users.noreply.github.com>
2024-03-07 15:29:48 +0900

    근희 코드와 병합
    근희의 Game 씬 수정 코드와 병합 완료
    
    + Lobby씬의 CharacterPage의 버튼 함수 재설정 필요
    
    + 뒤로가기 버튼 누를 때 배경음악 다시 재생되는 현상도 확인 필요

--- Assets/Game/Scripts/Managers/LobbyManager.cs
+++ Assets/Game/Scripts/Managers/LobbyManager.cs
@@ -26,8 +26,11 @@ public class LobbyManager : MonoBehaviour
     // Exit 버튼
     public UnityEngine.UI.Button ExitButtonObject;
 
-    // Option 버튼
-    public UnityEngine.UI.Button OptionButtonObject;
+    // Main Page Option 버튼
+    public UnityEngine.UI.Button MainPageOptionButtonObject;
+
+    // Character Page Option 버튼
+    public UnityEngine.UI.Button CharacterPageOptionButtonObject;
 
     // Character Page 오브젝트
     public GameObject CharacterPage;
@@ -45,6 +48,9 @@ public class LobbyManager : MonoBehaviour
     public GameObject SelectAssassin; // Assasin
     public GameObject SelectMage; // Mage
     public GameObject SelectWarrior; // Warrior
+
+    // 캐릭터 선택창이 켜졌는지를 확인할 때 필요한 bool 변수
+    bool isCharacterSelect = false;
 
     private void Awake()
     {
@@ -86,9 +92,12 @@ public class LobbyManager : MonoBehaviour
         UnityEngine.UI.Button SettingPageBackButton = SettingPageBackButtonObject.GetComponent<UnityEngine.UI.Button>();
         SettingPageBackButton.onClick.AddListener(SettingPageBackButtonClicked);
 
-        // Option 버튼 눌렀을 때
-        UnityEngine.UI.Button OptionButton = OptionButtonObject.GetComponent<UnityEngine.UI.Button>();
-        OptionButton.onClick.AddListener(OptionButtonClicked);
+        // MainPage의 Option 버튼 눌렀을 때
+        UnityEngine.UI.Button Main_OptionButton = MainPageOptionButtonObject.GetComponent<UnityEngine.UI.Button>();
+        Main_OptionButton.onClick.AddListener(Main_OptionButtonClicked);
+
+        UnityEngine.UI.Button CharacterPage_OptionButton = CharacterPageOptionButtonObject.GetComponent<UnityEngine.UI.Button>();
+        CharacterPage_OptionButton.onClick.AddListener(CharacterPage_OptionButtonClicked);
 
         // 현재 구현 1개 - Mage
         // Mage 선택 시
@@ -114,6 +123,8 @@ public class LobbyManager : MonoBehaviour
     {
         //lobbyAudioManager.PlaySfx(LobbyAudioManager.Sfx.Select);
         CharacterExplainGroup.SetActive(true);
+
+        isCharacterSelect = true;
 
         // 추가사항 필요 - GameStart 버튼 활성화
         gameStartButtonObject.interactable = true;
@@ -145,10 +156,26 @@ public class LobbyManager : MonoBehaviour
     }
 
     // Character Page 뒤로가기 버튼 클릭 시
+    // 1. CharacterPage_OptionButtonClicked() 호출됐을 때
+    // 2. CharacterPage_OptionButtonClicked() 호출되지 않을 때(아무것도 선택 x)
     private void CBackButtonClicked()
     {
         //lobbyAudioManager.PlaySfx(LobbyAudioManager.Sfx.Select); 
-        CharacterPage.SetActive(false);
+        //CharacterPage.SetActive(false);
+
+        if(isCharacterSelect) // Character 선택 후 스토리 패널 표시 돼있는 경우
+        {
+            CharacterExplainGroup.SetActive(false);
+            isCharacterSelect = false;
+        }
+        else // 캐릭터 선택 안되고 MainPage로 갈 상황인 경우
+        {
+
+            CharacterPage.SetActive(false);
+            isCharacterSelect = false;
+        }
+
+        
     }
 
     // Item Page 뒤로가기 버튼 클릭 시
@@ -169,7 +196,8 @@ public class LobbyManager : MonoBehaviour
         ExitButtonObject.enabled = true;
     }
 
-    private void OptionButtonClicked()
+    // Main Page에 있는 Option 버튼 클릭 시
+    private void Main_OptionButtonClicked()
     {
         //lobbyAudioManager.PlaySfx(LobbyAudioManager.Sfx.Select);
         SettingPage.SetActive(true);
@@ -177,6 +205,15 @@ public class LobbyManager : MonoBehaviour
         // 다른 버튼들 비활성화
         CharacterButtonObject.enabled = false; // Character 버튼 비활성화
         ExitButtonObject.enabled = false; // Exit 버튼 비활성화
-
     }
+
+    private void CharacterPage_OptionButtonClicked()
+    {
+         // 뭐지 ? 왜 Inspector창에서 한 것처럼 Button 자체가 꺼졌다 켜지지 ?? Button의 interactable이 꺼졌다 켜져야하는...
+        gameStartButtonObject.interactable = false; 
+    }
+
+    // 추가적으로 해야할 상황
+    // CharacterPage의 옵션에서 뒤로가기 버튼 누를 때 gameStartButtonObject interactable을 true로 변경할 것!
+    // CharacterPage의 뒤로가기 버튼, 상황별 코딩
 }
